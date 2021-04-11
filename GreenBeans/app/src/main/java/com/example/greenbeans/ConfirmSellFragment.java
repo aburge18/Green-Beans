332539b57fb8196f76606d4e99486af3d90b4a6e
nspen99 <47644537+nspen99@users.noreply.github.com>
2021-04-11 17:57:19 -0400

    Cleaned up most code and added documentation. Updated classes to hold most functions

---
+++ GreenBeans/app/src/main/java/com/example/greenbeans/ConfirmSellFragment.java
@@ -0,0 +1,166 @@
+package com.example.greenbeans;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+public class ConfirmSellFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    TextView confirmTV;
+    Double priceVal;
+    Position tempPosition;
+    String symbol;
+    String quantity;
+
+    public ConfirmSellFragment() {
+        // Required empty public constructor
+    }
+
+    public static ConfirmSellFragment newInstance(String param1, String param2) {
+        ConfirmSellFragment fragment = new ConfirmSellFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_confirm_sell, container, false);
+        confirmTV = view.findViewById(R.id.confirmSellMsgTV);
+        quantity = mListener.getConfirmBuyQuantity();
+        tempPosition = mListener.getPositionToBuy();
+        symbol = tempPosition.symbol;
+
+        new GetPrice().execute();//get most recent stock price
+
+        Button confirmBtn = view.findViewById(R.id.confirmSellBtn);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    new BuyPosition().execute();//start buy order
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Button cancelBtn = view.findViewById(R.id.cancelConfirmSellBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().popBackStack();
+            }
+        });
+        return view;
+    }
+
+
+    public class GetPrice extends AsyncTask<Integer, Double, String> implements Runnable {
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public String doInBackground(Integer... values) {
+            String price;
+            tempPosition.setCurrentPrice();
+            price = tempPosition.currentPrice;
+            return price;
+        }
+
+        protected void onProgressUpdate(Double... values) {}
+
+        protected void onPostExecute(String price) {//when doInBackground is done executing
+            super.onPostExecute(price);
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String confirmStr = "Sell: " + quantity + " of " + symbol + " for $" + price;
+                    confirmTV.setText(confirmStr);
+                }
+            });
+        }
+        @Override
+        public void run() {}
+    }
+    public class BuyPosition extends AsyncTask<Integer, Double, Position> implements Runnable {//buys current quantity of current position
+
+        public BuyPosition() throws JSONException {
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public Position doInBackground(Integer... values) {
+
+            Account account = mListener.getCurrentAccount();
+            try {
+                account.buyPosition(quantity, tempPosition);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return new Position();//send list of nums to postExecute
+        }
+
+        protected void onProgressUpdate(Double... values) {
+
+        }
+
+        protected void onPostExecute(Position tempPosition) {//when doInBackground is done executing
+            super.onPostExecute(tempPosition);
+            System.out.println("ADDed: " + tempPosition.symbol);
+        }
+
+        @Override
+        public void run() {}
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context){
+        super.onAttach(context);
+        if (context instanceof IListener){
+            mListener = (IListener)context;
+        }else{
+            throw new RuntimeException(context.toString() + " must implement listener");
+        }
+    }
+
+    IListener mListener;
+
+    public interface IListener{
+        Account getCurrentAccount();
+        String getConfirmBuyQuantity();
+        Position getPositionToBuy();
+    }
+}
---
+++ GreenBeans/app/src/main/java/com/example/greenbeans/SellPositionFragment.java
@@ -0,0 +1,193 @@
+package com.example.greenbeans;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SellPositionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SellPositionFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    TextView buyPriceTV;
+    Double priceVal;
+    String positionSymbol;
+    Double quantity ;
+    Position currentPosition;
+    public SellPositionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SellPositionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SellPositionFragment newInstance(String param1, String param2) {
+        SellPositionFragment fragment = new SellPositionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sell_position, container, false);
+        EditText quatityETV = view.findViewById(R.id.buyQuantityETV);
+        TextView buySymobolTV = view.findViewById(R.id.buySymbolTV);
+        TextView buyAmountTV = view.findViewById(R.id.buyAmountTV);
+        TextView buyPositionTV = view.findViewById(R.id.buyPositionTV);
+        EditText searchETV = view.findViewById(R.id.buySearchETV);
+        TextView buyTotalTV = view.findViewById(R.id.buyTotalTV);
+        buyPriceTV = view.findViewById(R.id.buyPriceTV);
+        currentPosition = mListener.getPositionToBuy();
+
+        quantity = 0.0;
+        if(currentPosition != null){//if position to buy was already specified
+            new GetPositionPrice().execute();
+            positionSymbol = currentPosition.symbol;
+        }
+
+        searchETV.setOnFocusChangeListener(new View.OnFocusChangeListener() {//if user clicks or unclicks search feature
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!searchETV.getText().toString().matches("")) {//search bar is not empty
+
+                    currentPosition = new Position();//create new position with specified symbol
+                    currentPosition.symbol = searchETV.getText().toString().toUpperCase();
+                    new GetPositionPrice().execute();//get the positions current price
+                    positionSymbol = searchETV.getText().toString();
+                    buySymobolTV.setText(positionSymbol);
+                    buyPositionTV.setText(positionSymbol);
+                }
+            }
+        });
+
+        Button buyPositionButton = view.findViewById(R.id.buyPositionBtn);
+        buyPositionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (quantity > 0){
+                    Position tempPosition = new Position();
+                    tempPosition.symbol = buySymobolTV.getText().toString();
+                    mListener.confirmBuy(quantity, tempPosition);
+                }
+            }
+        });
+
+        buySymobolTV.setText(positionSymbol);
+        quatityETV.setText("0");
+        buyPositionTV.setText(positionSymbol);
+        buyAmountTV.setText("0");
+        buyTotalTV.setText("0.00");
+
+        quatityETV.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!quatityETV.getText().toString().matches("")) {
+                    DecimalFormat df = new DecimalFormat("0.00");
+                    quantity = Double.valueOf(quatityETV.getText().toString());
+                    buyAmountTV.setText(quantity.toString());
+                    Double total = quantity * priceVal;
+                    buyTotalTV.setText(df.format(total));
+                }
+            }
+        });
+        return view;
+    }
+    public class GetPositionPrice extends AsyncTask<Integer, Double, String> implements Runnable {
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public String doInBackground(Integer... values) {
+            currentPosition.setCurrentPrice();
+            return currentPosition.currentPrice;
+        }
+
+        protected void onProgressUpdate(Double... values) {}
+
+        protected void onPostExecute(String price) {//when doInBackground is done executing
+            super.onPostExecute(price);
+
+            System.out.println("Price:  " + price);
+            priceVal = Double.valueOf(price);
+            if(getActivity() == null) {
+                return;
+            }
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    buyPriceTV.setText(price);
+                }
+            });
+        }
+
+        @Override
+        public void run() {}
+    }
+    @Override
+    public void onAttach(@NonNull Context context){
+        super.onAttach(context);
+        if (context instanceof IListener){
+            mListener = (IListener)context;
+        }else{
+            throw new RuntimeException(context.toString() + " must implement listener");
+        }
+    }
+
+   IListener mListener;
+
+    public interface IListener{
+        Position getPositionToBuy();
+        void confirmBuy(Double quantity, Position position);
+    }
+}
